483cf12582bfc02b3ebd53fc084a5dd4e2834943
Catwallon <catwallon@proton.me>
2024-10-18 13:43:28 +0200

    + | Added battery notification script

--- .scripts/battery-notifications.sh
+++ .scripts/battery-notifications.sh
@@ -5,31 +5,39 @@ CRITICAL_BATTERY_LEVEL=5
 
 LOW_BATTERY_ICON=/usr/share/icons/breeze-dark/status/32/battery-010.svg
 CRITICAL_BATTERY_ICON=/usr/share/icons/breeze-dark/status/32/battery-000.svg
+CHARGING_ICON=/usr/share/icons/breeze-dark/preferences/32/preferences-desktop-thunderbolt.svg
 
 low_notified=false
 critical_notified=false
+charging_notified=false
 
 while true; do
     BATTERY_LEVEL=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+    CHARGING=$(acpi -b | grep -q "Charging" && echo true || echo false)
 
-    if [ "$BATTERY_LEVEL" -le "$LOW_BATTERY_LEVEL" ] && [ "$BATTERY_LEVEL" -gt "$CRITICAL_BATTERY_LEVEL" ]; then
-        if [ "$low_notified" = false ]; then
+    if [ "$CHARGING" = true ]; then
+        if [ "$charging_notified" = false ]; then
+            dunstify -u normal -i "$CHARGING_ICON" "Charging" "Power adapter plugged"
+            low_notified=false
+            critical_notified=false
+            charging_notified=true
+        fi
+    else
+        charging_notified=false
+
+        if [ "$BATTERY_LEVEL" -le "$CRITICAL_BATTERY_LEVEL" ] && [ "$critical_notified" = false ]; then
+            dunstify -u critical -i "$CRITICAL_BATTERY_ICON" "Critical battery" "Battery at $BATTERY_LEVEL%"
+            critical_notified=true
+        elif [ "$BATTERY_LEVEL" -le "$LOW_BATTERY_LEVEL" ] && [ "$critical_notified" = false ] && [ "$low_notified" = false ]; then
             dunstify -u normal -i "$LOW_BATTERY_ICON" "Low battery" "Battery at $BATTERY_LEVEL%"
             low_notified=true
         fi
-        critical_notified=false
-    else
-        low_notified=false
-    fi
 
-    if [ "$BATTERY_LEVEL" -le "$CRITICAL_BATTERY_LEVEL" ]; then
-        if [ "$critical_notified" = false ]; then
-            dunstify -u critical -i "$CRITICAL_BATTERY_ICON" "Critical battery" "Battery at $BATTERY_LEVEL%"
-            critical_notified=true
+        if [ "$BATTERY_LEVEL" -gt "$LOW_BATTERY_LEVEL" ]; then
+            low_notified=false
+            critical_notified=false
         fi
-    else
-        critical_notified=false
     fi
 
-    sleep 60
+    sleep 1
 done
